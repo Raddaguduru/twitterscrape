baf6b42bfd8270d8e28e32f8fb93e1eacf2e0f6b
Raddaguduru <raviteja0628@gmail.com>
2022-07-11 20:48:17 -0500

    new file

--- import twint.py
+++ import twint.py
@@ -2,8 +2,10 @@ import twint
 
 # Configure
 c = twint.Config()
-c.Username = "realDonaldTrump"
-c.Search = "great"
+c.Username = "nickgerli1"
+c.Search = "recession"
+c.Output = "tweets.csv"
+c.Store_csv = True
 
 # Run
 twint.run.Search(c)
